5fa2fa7f3b213acbeb6f56814bed15568ffa8be8
Eddy Chase <eddykanyz.ek@gmail.com>
2020-11-09 22:59:31 +0300

    Project publication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { FormComponent } from './form/form.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    FormComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
